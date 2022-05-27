6cb9d0c889daec7dcba670ed1e93d8a73d59c5cc
Junior Sott <sottjr@gmail.com>
2022-05-28 01:09:35 +0200

    Upando arquivos TS

---
+++ src/domain/psicologo/services/PsicologoService.ts
@@ -0,0 +1,25 @@
+import { viaCepApi } from "../../../infrastructure/viaCEP";
+import { Psicologos } from "../models/psicologos";
+import bcrypt from "bcryptjs";
+
+export class PsicologoService {
+  async registerPsicologo(data: any) {
+    const { senha, cep } = data;
+
+    
+
+    const newSenha = bcrypt.hashSync(senha, 10);
+
+    const newPsicologo = await Psicologos.create({
+      ...data,
+      senha: newSenha,
+      bairro: this.getBairro(cep)
+    });
+  }
+
+  async getBairro (cep: string) {
+    const fullAddress = await viaCepApi.getAddress(cep);
+    return fullAddress.bairro;
+  }
+
+}
